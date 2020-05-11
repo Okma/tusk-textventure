f68905dd2d4968c4251a8cfbb63d9bfbd23f3a85
Carl <carl.amko@twosixlabs.com>
2020-05-11 18:11:09 -0400

    Update scoreboard style

--- src/component/Scoreboard.js
+++ src/component/Scoreboard.js
@@ -1,15 +1,46 @@
 import React from "react";
 import './Scoreboard.css';
+import confetti from 'canvas-confetti';
 
 export default class Scoreboard extends React.Component {
+
+    playConfetti = () => {
+        function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        const duration = 5 * 1000; // ms
+        const animationEnd = Date.now() + duration;
+        const defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 0};
+
+        const interval = setInterval(function () {
+            const timeLeft = animationEnd - Date.now();
+
+            if (timeLeft <= 0) {
+                return clearInterval(interval);
+            }
+
+            const particleCount = 200 * (timeLeft / duration);
+            // since particles fall down, start a bit higher than random
+            confetti(Object.assign({}, defaults, {
+                particleCount,
+                origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2}
+            }));
+            confetti(Object.assign({}, defaults, {
+                particleCount,
+                origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2}
+            }));
+        }, 250);
+    };
 
     calculateScore = () => {
         let score = 0;
 
         // calculate credit score
         let credits = this.props.credits;
-        let creditScores = {50000: 4000, 40000: 2000, 45000: 3000, 35000: 1000};
-        let cutoffs = Object.keys(creditScores);
+        let creditScores = {50000: 4000, 45000: 3000, 40000: 2000, 35000: 1000};
+        // sort keys in descending order
+        let cutoffs = Object.keys(creditScores).reverse();
         for (let i = 0; i < cutoffs.length; i++) {
             if (credits >= cutoffs[i]) {
                 score += creditScores[cutoffs[i]];
@@ -30,7 +61,7 @@ export default class Scoreboard extends React.Component {
         // calculate health score
         let health = this.props.health;
         let healthScores = {1: 2000, 26: 4000, 51: 8000, 76: 10000};
-        // by default sorts keys in ascending order
+        // sort keys in descending order
         cutoffs = Object.keys(healthScores).reverse();
         for (let i = 0; i < cutoffs.length; i++) {
             let healthCheck = cutoffs[i];
@@ -40,40 +71,63 @@ export default class Scoreboard extends React.Component {
             }
         }
 
-        let title;
+        return score;
+    };
+
+    start = (score) => {
+        // console.log(score);
+
+        // end with confetti
+        this.playConfetti();
+    };
+
+    render() {
         let titles = {
             16000: 'Outlawed Cosmonaut',
             11000: 'Maurading Rocketeer',
             6000: 'Burgling Pilot',
-            1: 'Pick Pocketing Star Gazer'
+            1000: 'Pick Pocketing Star Gazer'
         };
-        // by default sorts keys in ascending order
-        cutoffs = Object.keys(titles).reverse();
-        for (let i = 0; i < cutoffs.length; i++) {
-            if (score >= cutoffs[i]) {
-                title = titles[cutoffs[i]];
-                break;
-            }
-        }
 
-        return {'score': score, 'title': title};
-    };
-
-    render() {
         let score = this.calculateScore();
+        this.start(score);
+
+        let keys = Object.keys(titles).reverse().map((n) => parseInt(n));
+        let target = keys.find((cutoff) => score >= cutoff);
         return (
             <>
                 <div className={'container'}>
                     <h2>Congratulations!</h2>
-                    {/*<table>
-                        <tr><td>Outlawed Cosmonaut</td><td>16000</td></tr>
-                        <tr><td>Maurading Rocketeer</td><td>11000</td></tr>
-                        <tr><td>Burgling Pilot</td><td>6000</td></tr>
-                        <tr><td>Pick Pocketing Star Gazer</td><td>1000</td></tr>
+                    {/*<table className={'table'}>
                     </table>*/}
-                    <p className={'score'}>{score.score}</p>
-                    <p className={'rank'}><br/>Your rank
-                        is <span>{score.title}!</span></p>
+                    <table className={'table score-table'}>
+                        <thead>
+                        <tr>
+                            <th scope="col">Rank</th>
+                            <th scope="col">Score Requirement</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {keys.map((cutoff) =>
+                            <tr key={cutoff}>
+                                {target === cutoff &&
+                                <th className={'title-active'}
+                                    scope={'row'}>{score >= cutoff ? titles[cutoff] : "???"}</th>}
+                                {target !== cutoff &&
+                                <td className={target > cutoff ? 'title-inactive' : ''}>
+                                    {score >= cutoff ? titles[cutoff] : "???"}
+                                </td>
+                                }
+                                <td>{cutoff}</td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                    {target !== 16000 ?
+                        <div className={'helper'}>Play again for a higher score!</div> :
+                        <div className={'helper'}>Good job! You reached the highest possible
+                            score!</div>
+                    }
                 </div>
             </>
         );
